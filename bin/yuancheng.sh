dffeccaf94e07fc9fa35e98df2277d1394084505
qinweijunlin <qinweijunlin@qq.com>
2019-04-19 23:51:47 +0800

    发布

--- bin/yuancheng.sh
+++ bin/yuancheng.sh
@@ -12,21 +12,56 @@ rootdir="${bindir%%/yuancheng*}"
 readonly rootdir
 
 #### 常量 ####
+readonly E_UNRECOGNIZED_OPTION=2
 
 #### 包含 ####
 
 #### 函数 ####
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
 
 #### 变量 ####
+readonly oneself="$0"
+readonly projectnum="$1"
 
 #### 主体 ####
-
-
-
-
-
-
-
-
-
-
+case "${projectnum}" in
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  )
+    bash "${rootdir}"/remote/bin/remote.sh  1,2,3
+    ;;
+  *)
+    err "${oneself##*/}: unrecognized option '${projectnum}'"
+    exit "${E_UNRECOGNIZED_OPTION}"
+    ;;
+esac
