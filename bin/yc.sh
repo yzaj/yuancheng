cbaf47aa73d7e96d7206f78b5ccb634add176801
qinweijunlin <qinweijunlin@qq.com>
2019-04-20 09:53:18 +0800

    发布

--- bin/yc.sh
+++ bin/yc.sh
@@ -20,7 +20,11 @@ readonly rootdir
 #### 变量 ####
 
 #### 主体 ####
+# 读取配置文件
+filedir::config "qq_list" "[1-9][0-9,-]*[1-2,]" "${confdir}/remote.conf"
+qq_list=",${qq_list},"
 
+readonly qq_list
 
 
 
