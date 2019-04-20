e7ce178ef341af236997a0a59307f855baf511e1
qinweijunlin <qinweijunlin@qq.com>
2019-04-20 09:55:47 +0800

    发布

--- bin/yc.sh
+++ bin/yc.sh
@@ -14,10 +14,14 @@ readonly rootdir
 #### 常量 ####
 
 #### 包含 ####
+. "${rootdir}"/shell/lib/filedir.sh
 
 #### 函数 ####
 
 #### 变量 ####
+qq_list='3-1,3-2,4-1,4-2,5-1,5-2,6-1,6-2,7-1,7-2'
+
+readonly confdir="${rootdir}/remote/conf"
 
 #### 主体 ####
 # 读取配置文件
